8bce29a733694debd8308955a4ff95c71e94a1b0
Gabriele <142537944+gabstarr@users.noreply.github.com>
2024-10-09 08:47:48 -0300

    sxhy

--- perguntas.js
+++ perguntas.js
@@ -5,5 +5,43 @@ criaCartao(
 )
 
 criaCartao(
+ "historia",
+ "quando iniciou-se a segunda guerra mundial?",
+ "1 de setembro de 1939"
+)
 
+criaCartao(
+    "geografia",
+    "quantos paises tem no mundo?",
+    "193"
+)
+
+criaCartao(
+    "biologia",
+    "qual o maior animal do mundo?",
+    "baleia-azul"
+)
+
+criaCartao(
+    "quimica",
+    "qual o elemento mais perigoso?",
+    "chumbo"
+)
+
+criaCartao(
+    "lingua portuguesa",
+    "qual a obra do escritor machado de assis?",
+    "dom casmurro"
+)
+
+criaCartao(
+    "ingles",
+    "qual o verbo correto no presente para 'they'?",
+    "are"
+)
+
+criaCartao(
+    "artes",
+    "qual o artista mais famoso da epoca renascentista?",
+    "leonardo da vinci"
 )
